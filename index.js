2cdca752464f061c40b2dd858d2a10a25470e1c2
aschemily <aschemily@gmail.com>
2019-01-06 13:10:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+  if (distance > 42){
+    return distance - 42;
+    } else{
+      return 42 - distance;
+    }
+  }
+
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance) * 264
+}
+
+function distanceTravelledInFeet(start, finish){
+  if (start < finish){
+      return (finish - start) * 264
+  }
+  else {
+    return (start - finish) * 264
+  }
+}
+
+function calculatesFarePrice(start, finish){
+  const distance = distanceTravelledInFeet(start, finish)
+
+  if (distance <= 400){
+    return 0;
+  }
+  else if (distance > 400 && distance <= 2000){
+    return .02 * (distance - 400)
+  }
+  else if (distance > 2000 && distance < 2500){
+    return 25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+
+}
