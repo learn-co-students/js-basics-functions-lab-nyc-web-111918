3aab19d4badd0e64013f7197caea9eaaced471df
Matt Doering <ablueplatypus21@gmail.com>
2019-01-06 18:20:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber) {
+      return Math.abs(streetNumber - 42);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+   return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function distanceTravelledInFeet(street1, street2) {
+  return Math.abs((street1 - street2) * 264)
+}
+
+function calculatesFarePrice(street1, street2) {
+  if (distanceTravelledInFeet(street1, street2) > 2500) {
+    return 'cannot travel that far';
+  } else if (distanceTravelledInFeet(street1, street2) > 2000) {
+    return 25;
+  } else if (distanceTravelledInFeet(street1, street2) > 400 && distanceTravelledInFeet(street1, street2) < 2000) {
+    return (distanceTravelledInFeet(street1, street2) - 400) * 0.02;
+  } else if (distanceTravelledInFeet(street1, street2) < 400) {
+    return 0;
+  }
+
+}
